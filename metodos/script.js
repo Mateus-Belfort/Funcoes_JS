1fac4e2efc670adb5ffd9e05ee23f2e1508cdbe8
Mateus Belfort <mateusaguia96@gmail.com>
2023-06-27 22:07:40 -0300

    adição de novas funções
    descritas no h1 do html e no script

--- metodos/script.js
+++ metodos/script.js
@@ -1,5 +1,5 @@
 //Métodos são funções atreladas a objetos
-let pessoa = {
+/*let pessoa = {
   nome: "Mateus",
   idade: 28,
   //exemplo de metodo dentro de um objeto
@@ -12,5 +12,51 @@ let pessoa = {
 console.log(pessoa)
 //nesse caso o log é um metodo do objeto console
 
-
 pessoa.dizerOla()
+*/
+
+
+//Funções Recursivas
+//É uma função que chama ela mesma. executando varias vezes (efeito recursão)
+/*
+function dividir (num){
+  console.log(num)
+  if(num % 2 === 0){
+    dividir(num / 2)
+  }else{
+    return num
+  }
+}
+dividir(48)
+
+
+function dobrar(num){
+  console.log(num)
+  if(num <= 100){
+  dobrar(num * 2)
+  }else {
+    return num
+  }
+}
+
+dobrar(3)
+
+*/
+
+//fatorial de 5: !5 === 5 * 4 * 3 * 2 * 1 === 5 * !4
+function fatorial(num){
+  console.log("Numero: " + num)
+  if(num === 0){
+    return 1
+  }else if (num === 1){
+    return 1
+  } else {
+    console.log(num + " * !" + (num - 1))
+    return num * fatorial(num - 1)
+  }
+}
+
+console.log(fatorial(15))
+
+
+
